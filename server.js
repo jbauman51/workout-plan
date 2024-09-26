8be995356a7ab59076c86c7466f23470ebd394fa
John Bauman <jbauman@williamhill.us>
2024-09-26 11:39:13 -0400

    changes for netlify server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.post('/save', (req, res) => {
+    fs.writeFile('formData.json', JSON.stringify(req.body), (err) => {
+        if (err) {
+            res.status(500).send('Error saving data');
+        } else {
+            res.send('Data saved');
+        }
+    });
+});
+
+app.get('/load', (req, res) => {
+    fs.readFile('formData.json', 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error loading data');
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
